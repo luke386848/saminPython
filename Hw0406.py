3b152a7b929fa8c74dab2b162bcb722679265c09
luke386848 <128707072+luke386848@users.noreply.github.com>
2023-04-10 22:56:38 +0800

    Add files via upload

---
+++ Hw0406.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 10 21:33:19 2023
+
+@author: huan
+"""
+
+import random
+
+a = random.sample(range(1, 50), 6)# 從包含 1～49 數字的串列中，取出六個不重複的數字變成串列
+
+print(a)
+
+print()
+
+
